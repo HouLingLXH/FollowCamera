e9957fb243bd9ac0a1d7ca350cbb215355847d5f
HouLingSWJ <30954632+HouLingSWJ@users.noreply.github.com>
2017-12-14 18:44:40 +0800

    新增

---
+++ CameraMove/Editor/CameraFollowEditor.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof(CameraFollowCompnent))]
+public class CameraFollowEditor : Editor {
+
+    static Texture s_tex_near;
+    static Texture s_tex_far;
+
+
+    private Vector2 v2_conversionCenter_near;
+    private Vector2 v2_conversionCenter_far;
+    private Vector2 v2_conversionSize_near;
+    private Vector2 v2_conversionSize_far;
+
+    private CameraFollowCompnent compnent;
+
+    void OnSceneGUI()
+    {
+        compnent = target as CameraFollowCompnent;
+        GetData();
+        DrawUI();
+    }
+
+    //获取数据
+    private void GetData()
+    {
+        s_tex_near = compnent.tex_near;
+        s_tex_far = compnent.tex_far;
+
+        v2_conversionSize_near = compnent.v2_size_near;
+        v2_conversionSize_far = compnent.v2_size_far;
+
+        ConversionCenter();
+    }
+
+    //换算中心
+    private void ConversionCenter()
+    {
+        v2_conversionCenter_near = compnent.v2_center_near;
+        v2_conversionCenter_near = new Vector2(v2_conversionCenter_near.x + Screen.width * 0.5f, v2_conversionCenter_near.y  + Screen.height * 0.5f);
+
+        v2_conversionCenter_far = compnent.v2_center_far;
+        v2_conversionCenter_far = new Vector2(v2_conversionCenter_far.x + Screen.width * 0.5f, v2_conversionCenter_far.y + Screen.height * 0.5f);
+
+    }
+
+
+    //绘制UI
+    private void DrawUI()
+    {
+        Handles.BeginGUI();
+        GUI.DrawTexture(new Rect(v2_conversionCenter_far.x - v2_conversionSize_far.x, v2_conversionCenter_far.y - v2_conversionSize_far.y, v2_conversionSize_far.x * 2, v2_conversionSize_far.y *2), s_tex_far,ScaleMode.StretchToFill);
+        GUI.DrawTexture(new Rect(v2_conversionCenter_near.x - v2_conversionSize_near.x, v2_conversionCenter_near.y - v2_conversionSize_near.y, v2_conversionSize_near.x * 2, v2_conversionSize_near.y * 2), s_tex_near,ScaleMode.StretchToFill);
+
+        Handles.EndGUI();
+    }
+}
---
+++ CameraMove/Scripts/CameraFollowCompnent.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//[ExecuteInEditMode]
+public class CameraFollowCompnent : MonoBehaviour {
+
+    public Texture tex_near;
+    public Vector2 v2_center_near;
+    public Vector2 v2_size_near;
+
+
+    public Texture tex_far;
+    public Vector2 v2_center_far;
+    public Vector2 v2_size_far;
+
+    public GameObject target;
+
+	public Camera camera;
+    // Update is called once per frame
+
+    public void Init(GameObject go, Camera l_camera = null ) 
+    {
+        target = go;
+
+        if (l_camera != null)
+        {
+            camera = l_camera;
+        }
+
+    }
+	void Update () {
+
+        if (camera == null || target == null)
+        {
+            return;
+        }
+
+        Vector3 v3_screenPoint = camera.WorldToScreenPoint(target.transform.position);
+
+        camera.transform.position =Vector3.Lerp(camera.transform.position,  target.transform.position + new Vector3(-8.8f, 2.51f, 5.1f),Time.deltaTime);
+        camera.transform.eulerAngles = new Vector3(12.4f, 119, 0);
+
+
+
+
+	}
+
+
+}
